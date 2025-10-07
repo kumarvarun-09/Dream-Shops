45b69eb687eba57d7a2b90bb4b1e67403f4ca01d
Varun Kumar <varunkr2109@gmail.com>
2025-10-08 00:11:16 +0530

    reltion between role and user created

---
+++ src/main/java/com/example/dreamshops/model/Role.java
@@ -0,0 +1,27 @@
+package com.example.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users = new HashSet<>();
+}
